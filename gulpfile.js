c2b5ad2b0c0434e1cdee544397d3c32cf3cda1b8
José Monar <jl_mj_17@hotmail.com>
2021-06-13 16:02:16 -0500

    Added festival information section, and implemented gulp task to minify images with gulp-imagemin dependency.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const { src, dest, watch } = require("gulp");
 const sass = require("gulp-sass");
+const imagemin = require("gulp-imagemin");
 
 sass.compiler = require("dart-sass");
 
@@ -21,6 +22,11 @@ function watchFiles() {
     watch("./src/scss/**/*.scss", css);
 }
 
+function minifyImage() {
+    return src("./src/img/**/*").pipe(imagemin()).pipe(dest("./build/img"));
+}
+
 exports.css = css;
 exports.minifyCSS = minifyCSS;
+exports.minifyImage = minifyImage;
 exports.watch = watchFiles;
