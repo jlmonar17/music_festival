ab5c30978788d19b2ed0a31d07c986e20005112c
José Monar <jl_mj_17@hotmail.com>
2021-06-14 15:18:54 -0500

    Applyng scroll view to jump to specifics sections.

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    scrollNav();
+});
 
+function scrollNav() {
+    const links = document.querySelectorAll(".nav-principal a");
+
+    links.forEach(function (link) {
+        console.log(link.attributes.href.value);
+        link.addEventListener("click", function (e) {
+            e.preventDefault();
+
+            const hrefLinkValue = e.target.attributes.href.value;
+            const section = document.querySelector(hrefLinkValue);
+
+            section.scrollIntoView({
+                behavior: "smooth",
+            });
+        });
+    });
+}
