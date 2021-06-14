be4cae68aec757c21720095a5e1162bd34f6ff92
José Monar <jl_mj_17@hotmail.com>
2021-06-14 16:15:38 -0500

    Using Intersection Observer API to observe when a section is visible, and fixed header when not visible.

--- build/js/bundle.js
+++ build/js/bundle.js
@@ -1,12 +1,30 @@
 document.addEventListener("DOMContentLoaded", function () {
     scrollNav();
+
+    fixedNavigation();
 });
+
+function fixedNavigation() {
+    const header = document.querySelector(".header");
+
+    // Register Intersection Observer
+    const observer = new IntersectionObserver(function (entries) {
+        if (entries[0].isIntersecting) {
+            header.classList.remove("fixed");
+        } else {
+            header.classList.add("fixed");
+        }
+    });
+
+    // Adding element to observe
+    const sectionFestival = document.querySelector(".about-festival");
+    observer.observe(sectionFestival);
+}
 
 function scrollNav() {
     const links = document.querySelectorAll(".nav-principal a");
 
     links.forEach(function (link) {
-        console.log(link.attributes.href.value);
         link.addEventListener("click", function (e) {
             e.preventDefault();
 
